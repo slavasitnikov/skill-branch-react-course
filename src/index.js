013bae0fb35936079256cea2bcc106b6bd2eae13
zorak <slavasitnikov8@gmail.com>
2016-11-19 22:56:21 +0300

    2A: done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-function hello(name) {
-  console.log(`Hello ${name}`); // eslint-disable-line
-}
-hello('JS World');
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.get('/', (req, res) => {
+  let a = req.query.a;
+  let b = req.query.b;
+
+  if (isNaN(a)) a = 0;
+  if (isNaN(b)) b = 0;
+
+  const result = +a + +b;
+  res.send(result.toString());
+});
+
+app.listen(3000, () => {
+  console.log('Your app listening on port 3000!');
+});
